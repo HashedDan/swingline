291075b409fba476a136d4c891892bec80143e76
HashedDan <georgedanielmangum@gmail.com>
2018-06-30 22:12:20 -0600

    messages no longer delivered to sending client

--- client/client.go
+++ client/client.go
@@ -13,12 +13,17 @@ func main() {
 		os.Exit(1)
 	}
 
+	go func() {
+		for {
+			message, _ := bufio.NewReader(conn).ReadString('\n')
+			fmt.Print("Receiving from server: " + message)
+		}
+	}()
+
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Sending to server: ")
 		text, _ := reader.ReadString('\n')
 		fmt.Fprintf(conn, text+"\n")
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Receiving from server: " + message)
 	}
 }
---
+++ server/server.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+var clientNodes map[*Client]net.Addr
+
+type Client struct {
+	address    net.Addr
+	outbound   chan string
+	conn       net.Conn
+	reader     *bufio.Reader
+	writer     *bufio.Writer
+	connection *Client
+}
+
+func (client *Client) Read() {
+	for {
+		message, _ := client.reader.ReadString('\n')
+		fmt.Print("From client "+client.address.String()+": ", string(message))
+		for node := range clientNodes {
+			if node.address != client.address {
+				node.outbound <- message
+			}
+		}
+	}
+}
+
+func (client *Client) Write() {
+	for data := range client.outbound {
+		client.writer.WriteString(data)
+		client.writer.Flush()
+	}
+}
+
+func (client *Client) Listen() {
+	go client.Read()
+	go client.Write()
+}
+
+func CreateClient(conn net.Conn) *Client {
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(conn)
+
+	client := &Client{
+		outbound: make(chan string),
+		conn:     conn,
+		reader:   reader,
+		writer:   writer,
+		address:  conn.RemoteAddr(),
+	}
+
+	client.Listen()
+
+	return client
+}
+
+func main() {
+	clientNodes = make(map[*Client]net.Addr)
+	fmt.Println("Server starting...")
+	ln, _ := net.Listen("tcp", ":8081")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			os.Exit(1)
+		}
+		client := CreateClient(conn)
+		clientNodes[client] = client.address
+		fmt.Println("New connection from: " + client.address.String())
+	}
+}
